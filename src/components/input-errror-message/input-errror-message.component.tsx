4b5072fb02b837a3e11a963aa2ae41ae35444301
Matheus Lopes Marendino <91556477+lopesmatheus1@users.noreply.github.com>
2024-11-14 14:29:44 -0300

    feat: add input-error-message component

---
+++ src/components/input-errror-message/input-errror-message.component.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react'
+import { InputErrorMessageContainer } from './input-error-message.styles'
+
+interface InputErrorMessageProps {
+  children: ReactNode
+}
+
+const InputErrorMessage = ({ children }: InputErrorMessageProps) => {
+  return <InputErrorMessageContainer>{children}</InputErrorMessageContainer>
+}
+
+export default InputErrorMessage
